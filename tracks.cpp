e77aa56e25f5a4401ab1bcbd3c3212e00cf9bfe1
Kayleigh Excell <kexcell@wisc.edu>
2026-05-05 09:46:25 -0500

    Structuring the project

---
+++ tracks.cpp
@@ -0,0 +1,28 @@
+#include "tracks.h"
+
+void read_data(){
+
+
+}
+
+
+void clustering(){
+
+
+}
+
+void kalman(){
+
+
+}
+
+
+void tracks(dint16_t A[N], dint16_t B[N], dint16_t C[N]){
+
+read_data;
+clustering;
+kalman;
+
+return
+
+}
---
+++ tracks_tb.cpp
@@ -0,0 +1,16 @@
+#include "tracks.h"
+
+int main(){
+
+//open data file
+
+
+//call function on data file
+tracks()
+
+//check if output seems real
+//if cool: return 0
+//else: return not 0
+
+
+}
